f40f54505ab386a8b7f64cd6281b56a9fbcda738
Diana Gren <dianagren@gmail.com>
2013-11-23 14:44:32 -0800

    Add datasource protocol.

--- DGCalendarView/DGCalendarView.h
+++ DGCalendarView/DGCalendarView.h
@@ -8,6 +8,16 @@
 
 #import <UIKit/UIKit.h>
 
+@class DGCalendarView;
+@class DGCalendarViewCell;
+@protocol DGCalendarViewDataSource <NSObject>
+@required
+- (DGCalendarViewCell *)calendarView:(DGCalendarView *)calendarView cellForItemAtIndexPath:(NSIndexPath *)indexPath;
+- (NSInteger)numberOfSectionsInCalendarView:(DGCalendarView *)calendarView;
+- (NSInteger)calendarView:(DGCalendarView *)calendarView numberOfItemsInSection:(NSInteger)section;
+@end
+
 @interface DGCalendarView : UIView
 
+@property (weak, nonatomic) id<DGCalendarViewDataSource> dataSource;
 @end
