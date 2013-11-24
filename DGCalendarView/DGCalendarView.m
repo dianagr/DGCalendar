ee39b32e2f11a036547b9b770dbbad71a5eda971
Diana Gren <dianagren@gmail.com>
2013-11-23 16:33:52 -0800

    Add y axis, inits.

--- DGCalendarView/DGCalendarView.m
+++ DGCalendarView/DGCalendarView.m
@@ -10,4 +10,32 @@
 
 @implementation DGCalendarView
 
+- (id)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        [self sharedInit];
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    if (self = [super initWithCoder:aDecoder]) {
+        [self sharedInit];
+    }
+    return self;
+}
+
+- (id)init {
+    if (self = [super init]) {
+        [self sharedInit];
+    }
+    return self;
+}
+
+- (void)sharedInit {
+    UIView *yAxis = [[UIView alloc] initWithFrame:CGRectMake(self.frame.origin.x+10, self.frame.origin.y + 20.0, 1.0, self.frame.size.height - 30.0)];
+    yAxis.backgroundColor = [UIColor darkGrayColor];
+    [self addSubview:yAxis];
+}
+
+
 @end
