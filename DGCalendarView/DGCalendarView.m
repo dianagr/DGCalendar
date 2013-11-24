f2f45fd2eceb0fdfa828d319542c4bf344d4e969
Diana Gren <dianagren@gmail.com>
2013-11-23 17:00:57 -0800

    Layout cells from dataSource in view.

--- DGCalendarView/DGCalendarView.m
+++ DGCalendarView/DGCalendarView.m
@@ -8,6 +8,7 @@
 
 #import "DGCalendarView.h"
 
+#import "DGCalendarViewCell.h"
 @implementation DGCalendarView
 
 - (id)initWithFrame:(CGRect)frame {
@@ -35,7 +36,34 @@
     UIView *yAxis = [[UIView alloc] initWithFrame:CGRectMake(self.frame.origin.x+10, self.frame.origin.y + 20.0, 1.0, self.frame.size.height - 30.0)];
     yAxis.backgroundColor = [UIColor darkGrayColor];
     [self addSubview:yAxis];
+
+    self.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.5];
 }
 
+- (void)layoutSubviews {
+    CGFloat y = 10.0;
+    CGFloat x = 10.0;
 
+    NSInteger items = [self _numberOfItems];
+    for (int i = 0; i < items; ++i) {
+        DGCalendarViewCell *cell = [self.dataSource calendarView:self cellForItemAtIndexPath:nil];
+        cell.frame = CGRectMake(x, y, 100, 100);
+        [self addSubview:cell];
+        y = CGRectGetMaxY(cell.frame) + 10;
+    }
+}
+
+- (NSInteger)_numberOfItems {
+    if ([self.dataSource respondsToSelector:@selector(calendarView:numberOfItemsInSection:)]) {
+        return [self.dataSource calendarView:self numberOfItemsInSection:0];
+    }
+    return 0;
+}
+
+- (NSInteger)_numberOfSections {
+    if ([self.dataSource respondsToSelector:@selector(numberOfSectionsInCalendarView:)]) {
+        return [self.dataSource numberOfSectionsInCalendarView:self];
+    }
+    return 1;
+}
 @end
